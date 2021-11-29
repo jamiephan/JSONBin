e2a58208a846856fb971d3ee5568461f3b61b258
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-11-29 21:10:39 +0800

    Added API route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const port = 8080
 app.use(express.json({ limit: "1mb" }))
 app.set("json spaces", 2)
 
+
+// API Route
+app.use("/api", require("./routes/api"))
+
+
+// Default 404 Route
 app.use((req, res) => {
     res.status(404).json({
         error: 404,
