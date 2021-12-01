6bf0985d42f3a713c6c32188dfef0c7e169726d8
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-12-01 19:23:24 +0800

    Updated bin.js to associate with api key owner

--- src/routes/api/bin.js
+++ src/routes/api/bin.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
+const apiKeyValidation = require('../../middleware/apiKeyValidation');
 const Bin = require('../../models/bin');
 
 // Create a Bin
-router.post("/", (req, res) => {
-    console.log(req.body);
+router.post("/", apiKeyValidation ,(req, res) => {
+
     const bin = new Bin({
         data: req.body,
+        user: req.user ? req.user : null
     })
     bin.save()
-    res.json(bin)
-    console.log(bin)
-    // console.log(nanoid(9));
+    res.json({
+        name: bin.name,
+        user: bin.user ? bin.user.email : null
+    })
 })
 
 module.exports = router
