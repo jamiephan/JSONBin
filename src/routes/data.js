554a331e90cafba3c67020340148b80451c6773a
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-12-02 15:05:06 +0800

    Added DELETE route to Bins

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const apiKeyValidation = require('../middleware/apiKeyValidation');
 const Bin = require("../models/bin");
 const NotFoundResponse = require("../Responses/NotFound");
+const InvalidApiKeyResponse = require('../responses/UserInvalidApiKey');
+const ServerErrorResponse = require("../Responses/ServerError");
 const router = express.Router();
 
+
+// Get Bin Data
 router.get("/:name", (req, res) => {
     Bin.findOne({ name: req.params.name }, (err, bin) => {
         if (!bin) {
@@ -13,5 +18,32 @@ router.get("/:name", (req, res) => {
         }
     })
 })
+
+// Delete Bin data, only admin and owner can delete
+router.delete("/:name", apiKeyValidation, (req, res) => {
+    // Check API Key
+    if (!req.user) return InvalidApiKeyResponse(res)
+
+    // Check if bin owner
+    Bin.findOne({ name: req.params.name })
+        .populate('user')
+        .exec((err, bin) => {
+            if (err) return ServerErrorResponse(res)
+            if (!bin) return NotFoundResponse(res)
+            
+            // Anonymous Bin
+            if (bin.user === null) return InvalidApiKeyResponse(res)
+
+            if (req.user.isAdmin || (bin.user.email === req.user.email)) {
+                bin.remove((err) => {
+                    if (err) return ServerErrorResponse(res)
+                    return res.json({ message: "Bin deleted" })
+                })
+            } else {
+                return InvalidApiKeyResponse(res)
+            }
+        })
+})
+
 
 module.exports = router
