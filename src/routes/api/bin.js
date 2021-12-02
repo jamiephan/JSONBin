2f01837575c897483fe468ef1bb92f1a2fd71d04
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-12-02 14:36:27 +0800

    Added extra check and return data

--- src/routes/api/bin.js
+++ src/routes/api/bin.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const apiKeyValidation = require('../../middleware/apiKeyValidation');
 const Bin = require('../../models/bin');
+const ServerErrorResponse = require('../../responses/ServerError');
 
 // Create a Bin
 router.post("/", apiKeyValidation ,(req, res) => {
@@ -9,10 +10,15 @@ router.post("/", apiKeyValidation ,(req, res) => {
         data: req.body,
         user: req.user ? req.user : null
     })
-    bin.save()
-    res.json({
-        name: bin.name,
-        user: bin.user ? bin.user.email : null
+    bin.save((err, bin) => {
+        if (err) {
+            return ServerErrorResponse(res)
+        }
+        res.json({
+            name: bin.name,
+            user: bin.user ? bin.user.email : null,
+            data: bin.data
+        })
     })
 })
 
