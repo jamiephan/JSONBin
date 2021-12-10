a6b239f7478759aab5d9d6a69751332db73094c5
Jamie Phan <9488815+jamiephan@users.noreply.github.com>
2021-12-10 06:23:20 +0000

    Updated debug/prune route

--- src/routes/api/debug.js
+++ src/routes/api/debug.js
@@ -3,20 +3,20 @@ const User = require('../../models/user');
 const Bin = require('../../models/bin');
 
 
-router.get("/prune", (req, res) => {
-    Bin.deleteMany({}, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("bin pruned")
-        }
-    })
-    User.deleteMany({}, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log("user pruned")
-        }
-    })
-})
+router.get("/prune", async (req, res) => {
+    try {
+        await User.deleteMany({});
+        console.log("Pruned User")
+        await Bin.deleteMany({});
+        console.log("Pruned Bin")
+        res.json({
+            message: "Successfully deleted all users and bins"
+        });
+    } catch(e){
+        console.log(e.message)
+        res.status(500).json({
+            message: e.message
+        });
+    }
+});
 module.exports = router
